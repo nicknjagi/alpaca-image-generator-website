95682becfc11691d6e45fcd2e9a7349973207266
nicknjagi <njaginicholas@gmail.com>
2023-02-23 15:52:17 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hairEl = document.querySelector('hair-el')
+const earsEl = document.querySelector('ears-el')
+const eyesEl = document.querySelector('eyes-el')
+const mouthEl = document.querySelector('mouth-el')
+const neckEl = document.querySelector('neck-el')
+const legEl = document.querySelector('leg-el')
+const accessoriesEl = document.querySelector('accessories-el')
+const backgroundEl = document.querySelector('background-el')
+
